310a2405c0a8c064d4ce09d1071693a4d81097b2
“dystechnic” <dystechnic@gmail.com>
2021-10-02 16:57:52 +0200

    minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,28 @@
 #include <EnableInterrupt.h>
 
 #define SERIAL_PORT_SPEED 9600
-#define RC_NUM_CHANNELS  2
 
-// inout channels from receiver
+#define RC_NUM_CHANNELS  2
 #define RC_CH1  0
 #define RC_CH2  1
-// #define RC_CH3  2
-// #define RC_CH4  3
 
-#define RC_CH1_INPUT  A0
-#define RC_CH2_INPUT  A1
-//  #define RC_CH3_INPUT  A2
-//  #define RC_CH4_INPUT  A3
+// pins receiver is connected to
+#define RC_CH1_INPUT  14  //  == pin A0
+#define RC_CH2_INPUT  15  //  == pin A1
 
+// definition of signal thresholds
 #define INPUT_LOW_THRESHOLD 1000 // Low to Mid input signal threshold. 
 #define INPUT_MID_THRESHOLD 1500 // Mid to High input signal threshold.
 
 //  Landing light settings
-//  #define LL_IRQ_NUMBER 0 // Interrupt number to use (0 = pin 2 on most boards)
-//  #define LL_PIN_SERVO 2 // Servo input pin number - this needs to match whatever interrupt is used
 #define LL_PIN_LIGHT 9 // Landing light output pin number
-//  #define LL_SERVO_THRESHOLD 1500 // Servo signal threshold to turn on/off landing light (pulse width in microseconds, 1000 to 2000)
-//  #define LL_SERVO_DEAD_ZONE 100 // Servo signal dead-zone size, eliminates flicker
-//  #define LL_SERVO_REVERSED true   // Whether or not the servo channel is reversed
 
 // Anti-collision beacon settings
 #define ACB1_PIN_LIGHT 6 // Pin number for anti-collision beacon 1
 #define ACB2_PIN_LIGHT 5 // Pin number for anti-collision beacon 2
-#define ACB_FADE_MIN 10 // Minimum fade level for beacon (0-255)
-#define ACB_FADE_MAX 75 // Maximum fade level for beacon (0-255)
-#define ACB_FADE_INTERVAL 30000 // Fade step interval, in microseconds (lower numbers = faster fade)
+#define ACB_FADE_MIN 10 // Minimum fade level for anti-collision beacon (0-255)
+#define ACB_FADE_MAX 75 // Maximum fade level for anti-collision beacon (0-255)
+#define ACB_FADE_INTERVAL 25000 // Fade step interval, in microseconds (lower numbers = faster fade)
 
 // Strobe settings
 #define STB_PIN_LIGHT 3 // Pin number for strobe light output
@@ -67,24 +59,22 @@ void calc_input(uint8_t channel, uint8_t input_pin) {
   }
 }
 
-void calc_ch1() { calc_input(RC_CH1, RC_CH1_INPUT); }
-void calc_ch2() { calc_input(RC_CH2, RC_CH2_INPUT); }
-// void calc_ch3() { calc_input(RC_CH3, RC_CH3_INPUT); }
-// void calc_ch4() { calc_input(RC_CH4, RC_CH4_INPUT); }
+void calc_ch1() { 
+  calc_input(RC_CH1, RC_CH1_INPUT); 
+}
+
+void calc_ch2() {
+  calc_input(RC_CH2, RC_CH2_INPUT); 
+}
 
 void debug() {
   Serial.print("CH1:"); Serial.print(rc_values[RC_CH1]); Serial.print("\t");
   Serial.print("CH2:"); Serial.println(rc_values[RC_CH2]);
-//  Serial.print("CH2:"); Serial.print(rc_values[RC_CH2]); Serial.println("\t");
-//  Serial.print("CH3:"); Serial.print(rc_values[RC_CH3]); Serial.print("\t");
-//  Serial.print("CH4:"); Serial.println(rc_values[RC_CH4]);
-//  delay(200);
 }
 
 // Turn on or off landing light
 void setLandingLight(boolean state)
 {
-//  float i;
   if (state && !curLandingLight) {
     digitalWrite(LL_PIN_LIGHT, HIGH);
   } else if (!state && curLandingLight) {
